4da95d0131e2f7146b06d2342f91b78148ec6578
Hope Walton <h_walton2@mail.fhsu.edu>
2025-01-29 17:28:05 -0800

    Initial commit

---
+++ view/form.php
@@ -0,0 +1,15 @@
+<main class="main-entry">
+    <h2 id="enterNumber">Enter a Number</h2>
+    <form action="<?php $_SERVER['PHP_SELF'] ?>" class="main-entry__form" method="GET">
+        <input 
+            class="main-entry__input"
+            id="num"
+            name="num"
+            aria-labelledby="enterNumber"
+            type="text"
+            maxlength="2"
+            autofocus
+            required>
+        <button class="main-entry__button">Go!</button>
+    </form>
+</main>
